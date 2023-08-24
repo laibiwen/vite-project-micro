9c56d6c52a48bdc05793ecf84990ab62632eb9e4
biwen.lai <biwen.lai@qizhidao.com>
2023-08-24 15:57:27 +0800

    feat: 环境变量

--- vite.config.ts
+++ vite.config.ts
@@ -4,29 +4,31 @@ import { fileURLToPath } from "url";
 import qiankun from "vite-plugin-qiankun";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  build: {
-    rollupOptions: {
-      output: {},
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "development" ? "/" : "/app-vue",
+    build: {
+      rollupOptions: {
+        output: {},
+      },
     },
-  },
-  plugins: [
-    vue(),
-    qiankun("app-vue", {
-      useDevMode: true,
-    }),
-  ],
-  server: {
-    port: 8089,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
+    plugins: [
+      vue(),
+      qiankun("app-vue", {
+        useDevMode: true,
+      }),
+    ],
+    server: {
+      port: 8089,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      origin: "http://localhost:8089",
     },
-    origin: "http://localhost:8089",
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
